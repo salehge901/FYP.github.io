b0f6c3276f0a195dcf62e2b33ad84c0ef7037692
ahsan781 <81172856+ahsan781@users.noreply.github.com>
2021-03-23 17:04:21 +0500

    new Pages
    signup login and schedule pages

--- fyproject/vib/urls.py
+++ fyproject/vib/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('contact', views.contact, name='contact'),
     path('login', views.login, name='login'),
     path('signup', views.signup, name='signup'),
+    path('Schedule', views.Schedule, name='Schedule'),
 ]
